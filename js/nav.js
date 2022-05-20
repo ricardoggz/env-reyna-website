b6fe3f6197f86644ae2ae18f3b4342e186ecdf61
ricardoggz <ricardo_ggzicm@outlook.com>
2022-05-19 22:19:24 -0500

    nav para mobile modificado

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,13 @@
-const navMenu = document.getElementById("nav_menu");
-const navBtn = document.getElementById("nav_btn");
+var navBtn = document.getElementById("nav_btn");
+var navMenu = document.getElementById("nav_menu");
+var btnOption = document.querySelectorAll(".btn_option");
 
 navBtn.addEventListener("click", function () {
   navMenu.classList.toggle("active");
+});
+
+btnOption.forEach((item) => {
+  item.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+  });
 });
